87b743819c8d95dddf175faf4f7ccf9af93decd4
Texicitys <texicitys@gmail.com>
2013-10-24 23:18:59 +0200

    uploader

---
+++ spec/dummy/db/schema.rb
@@ -0,0 +1,64 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20131018214212) do
+
+  create_table "app_files", force: true do |t|
+    t.integer "owner_id"
+    t.string  "owner_type"
+    t.integer "parent_id"
+    t.integer "lft"
+    t.integer "rgt"
+    t.integer "depth"
+  end
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups_users", id: false, force: true do |t|
+    t.integer "group_id", null: false
+    t.integer "user_id",  null: false
+  end
+
+  create_table "shares", force: true do |t|
+    t.integer "owner_id"
+    t.string  "owner_type"
+    t.integer "app_file_id"
+    t.boolean "can_create",  default: false
+    t.boolean "can_read",    default: false
+    t.boolean "can_update",  default: false
+    t.boolean "can_delete",  default: false
+    t.boolean "can_share",   default: false
+  end
+
+  create_table "shares_items", force: true do |t|
+    t.integer "share_id"
+    t.integer "item_id"
+    t.string  "item_type"
+    t.boolean "can_add",    default: false
+    t.boolean "can_remove", default: false
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "nickname"
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
