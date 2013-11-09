5187bebe901d9062d02b73f3f006db6dd074b3de
Texicitys <texicitys@gmail.com>
2013-11-09 19:43:08 +0100

    renome the base file

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "repository_manager"
+require "repository-manager"
 
 module Dummy
   class Application < Rails::Application
