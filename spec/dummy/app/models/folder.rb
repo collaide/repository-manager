5f40230cf8dfdd5fca17a734befa84525ed70d49
Texicitys <texicitys@gmail.com>
2013-11-09 14:10:25 +0100

    engine works

--- spec/dummy/app/models/folder.rb
+++
@@ -1,7 +0,0 @@
-class Folder < Repository
-
-  #Add a repository in the folder.
-  def addRepository(repository)
-    repository.update_attribute :parent, self
-  end
-end
--- spec/dummy/app/models/shares_item.rb
+++
@@ -1,9 +0,0 @@
-class SharesItem < ActiveRecord::Base
-
-  belongs_to :item, polymorphic: true
-  belongs_to :share
-
-  #can_add
-  #can_remove
-  #t.references :shareable, polymorphic: true
-end
