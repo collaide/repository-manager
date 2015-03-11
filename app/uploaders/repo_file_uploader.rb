4a008be268e3e9af44720c897de7c31d32472f45
facenord <facenord.sud@gmail.com>
2015-03-11 11:25:44 +0100

    added the ability to have different storage for Carrierwave

--- app/uploaders/repo_file_uploader.rb
+++ app/uploaders/repo_file_uploader.rb
@@ -11,7 +11,7 @@ class RepoFileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage RepositoryManager.storage
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
