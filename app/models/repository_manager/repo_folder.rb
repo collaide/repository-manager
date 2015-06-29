6c6423b268a9d7fcc69fe3f3449a56ee5d86a4ba
Daniel Hosterman <daniel.hosterman@gmail.com>
2015-06-29 13:27:57 -0400

    Create children correctly.

--- app/models/repository_manager/repo_folder.rb
+++ app/models/repository_manager/repo_folder.rb
@@ -14,10 +14,11 @@ class RepositoryManager::RepoFolder < RepositoryManager::RepoItem
       name = path_array[0]
       children = self.get_children_by_name(name)
       unless children
-        children = RepositoryManager::RepoFolder.new(name: name)
-        children.owner = options[:owner]
-        children.sender = options[:sender]
-        children.save!
+        (options[:owner] || self.owner).create_folder(name: name, source_folder: children.first)
+        # children = RepositoryManager::RepoFolder.new(name: name)
+        # children.owner = options[:owner]
+        # children.sender = options[:sender]
+        # children.save!
       end
       # remove the first element
       path_array.shift
